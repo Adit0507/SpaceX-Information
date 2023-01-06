5ddc7319adc978a05a5eb7044554d44e75dc7990
Adit0507 <adi05072002@gmail.com>
2023-01-06 13:35:33 +0530

    Done with Error page

---
+++ src/pages/Error.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+const Error = () => {
+  return (
+    <>
+      <section className="flex flex-col items-center justify-center text-center h-screen">
+        <h1>Error | The requested resource could not be found</h1>
+        <Link to="/" className="btn">
+          &larr; Back to HomePage
+        </Link>
+      </section>
+    </>
+  )
+}
+
+export default Error
