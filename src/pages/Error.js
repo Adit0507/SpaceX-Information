cea0c5d8e223622b361a45701c2b9568a52282ea
Adit0507 <adi05072002@gmail.com>
2023-01-06 13:37:37 +0530

    Small update

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -4,7 +4,7 @@ const Error = () => {
   return (
     <>
       <section className="flex flex-col items-center justify-center text-center h-screen">
-        <h1>Error | The requested resource could not be found</h1>
+        <h1 className="text-white">Error | The requested resource could not be found</h1>
         <Link to="/" className="btn">
           &larr; Back to HomePage
         </Link>
