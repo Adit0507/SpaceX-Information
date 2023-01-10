47d9baa208c3daa172a2794ff7294352ab7bd9df
Adit0507 <adi05072002@gmail.com>
2023-01-10 20:32:17 +0530

    Crew Page done

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,6 @@ import Error from "./Error";
 import Home from "./Home";
 import Capsules from "./Capsules"
 import Cores from "./Cores";
+import Crew from "./Crew";
 
-export {Error, Home, Capsules, Cores}
+export { Error, Home, Capsules, Cores, Crew }
