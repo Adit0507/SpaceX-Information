90fa19747591fb87c19f1a9cd191ba1078d66f13
Adit0507 <adi05072002@gmail.com>
2023-01-11 20:05:57 +0530

    Single Crew page done

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import Home from "./Home";
 import Capsules from "./Capsules"
 import Cores from "./Cores";
 import Crew from "./Crew";
+import SingleCrew from "./SingleCrew";
 
-export { Error, Home, Capsules, Cores, Crew }
+export { Error, Home, Capsules, Cores, Crew, SingleCrew }
