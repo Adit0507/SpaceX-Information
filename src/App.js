ce5e2be9bbd3258dffcb7be9d4f2ac70842916bb
Adit0507 <adi05072002@gmail.com>
2023-01-16 19:42:42 +0530

    Finished :fire:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { Error, Home, Launches, SingleLaunch, LaunchPads, SingleLaunchPad, Capsules, Cores, Crew, SingleCrew, Dragons, SingleDragon, Landpad, SingleLandPads, Roadster, Rocket, SingleRocket, Payload, Starlink } from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='font-bold underline text-5xl'>HBUE</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/capsules" element={<Capsules />} />
+        <Route path="/cores" element={<Cores />} />
+        <Route path="/crew" element={<Crew />} />
+        <Route path="/crew/:id" element={<SingleCrew />} />
+        <Route path="/dragons" element={<Dragons />} />
+        <Route path="/dragons/:id" element={<SingleDragon />} />
+        <Route path="/landpads" element={<Landpad />} />
+        <Route path="/landpads/:id" element={<SingleLandPads />} />
+        <Route path="/launches" element={<Launches />} />
+        <Route path="/launches/:id" element={<SingleLaunch />} />
+        <Route path="/launchpads" element={<LaunchPads />} />
+        <Route path="/launchpads/:id" element={<SingleLaunchPad />} />
+        <Route path="/payloads" element={<Payload />} />
+        <Route path="/roadster" element={<Roadster />} />
+        <Route path="/rockets" element={<Rocket />} />
+        <Route path="/rockets/:id" element={<SingleRocket />} />
+        <Route path="/starlink" element={<Starlink />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      {/* Ships pages */}
+      {/* Starlink pages */}
+    </BrowserRouter>
   );
 }
 
